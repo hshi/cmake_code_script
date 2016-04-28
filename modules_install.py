72e7622de119d4ab68fdda358e98c0447abd0a47
Hao Shi <boruoshihao@gmail.com>
2016-04-28 16:17:40 -0400

    Update modules_install.py

--- modules_install.py
+++ modules_install.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python
 import sys
+import os
 import subprocess
+import glob
 
 install_dirc_name = sys.argv[1]
-print "\033[1m" "Download custom cmake modules, move to "+install_dirc_name+"/Modules" "\033[0m"
-subprocess.call('git clone https://github.com/hshi/Modules', shell=True)
-subprocess.call('rm -rf '+install_dirc_name+'/Modules', shell=True)
-subprocess.call('mkdir '+install_dirc_name+'/Modules', shell=True)
-subprocess.call('cp -r Modules '+install_dirc_name, shell=True)
+
+print "\033[1m" "Download custom cmake modules, move to "+install_dirc_name+"/Modules." "\033[0m"
+str = raw_input("To comfirm, input 'Y':")
+
+if str=='Y':
+   subprocess.call('git clone https://github.com/hshi/Modules', shell=True)
+   subprocess.call('rm -rf '+install_dirc_name+'/Modules', shell=True)
+   subprocess.call('cp -r Modules '+install_dirc_name, shell=True)
+else:
+   print "Do not installed."
