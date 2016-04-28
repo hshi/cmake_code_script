e4be0b58fd4bf2f9f101a106c3f4281241793a02
Hao Shi <boruoshihao@gmail.com>
2016-04-28 11:03:29 -0400

    Update lib_install.py

--- lib_install.py
+++ lib_install.py
@@ -8,15 +8,16 @@ typ               = sys.argv[1]
 src_name          = sys.argv[2]
 install_dirc_name = sys.argv[3]+'/'+typ
 
-com='python ../cmake.py '+ typ
+cwd_path    = os.getcwd()
+script_path = os.path.abspath(os.path.dirname(__file__))
 
-path = os.path.abspath(os.path.dirname(__file__))
+com='python '+ script_path+'/cmake.py ' + typ
 
 subprocess.call('rm -rf build', shell=True)
 subprocess.call('mkdir build', shell=True)
 os.chdir('build')
 
-src_path_list=glob.glob(path+'/'+src_name)
+src_path_list=glob.glob(cwd_path+'/'+src_name)
 for src_path in src_path_list:
     subprocess.call('rm -rf *', shell=True)
     subprocess.call(com +' '+ src_path +' '+install_dirc_name, shell=True )
--- modules_install.py
+++ modules_install.py
@@ -3,7 +3,7 @@ import sys
 import subprocess
 
 install_dirc_name = sys.argv[1]
-print "\033[1m"'Dowload custom cmake modules, move to '+install_dirc_name+'/Modules' "\033[0m"
+print "\033[1m" "Download custom cmake modules, move to "+install_dirc_name+"/Modules" "\033[0m"
 subprocess.call('git clone https://github.com/hshi/Modules', shell=True)
 subprocess.call('rm -rf '+install_dirc_name+'/Modules', shell=True)
 subprocess.call('mkdir '+install_dirc_name+'/Modules', shell=True)
