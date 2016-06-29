8b6bada13a001fe9d7555a9b8f655e2d748d9ef0
Hao Shi <boruoshihao@gmail.com>
2016-06-29 15:57:08 -0400

    Update CMakeLists.txt

--- cmake.py
+++ cmake.py
@@ -3,6 +3,25 @@ import sys
 import os
 import subprocess
 
+#===============================================================================================================
+#BUILD YOUR OWN CMAKE.PY TO RUN CMAKE
+# PATH TO LIBRARY:    os.environ['MAGMA'] ="/ccs/home/hshi/magma/magma-1.6.1"
+# CHOOSE COMPILER:   -DCMAKE_CXX_COMPILER=mpic++
+# ADD COMPILER FLAG: -DCMAKE_CXX_FLAGS='-Wall -O3 -fopenmp'
+# ADD DEFINATION:    -DCOMPILER_EXTRA_DEF:STRING='-DMPI_HAO'   //NEED TO USE -DMPI_HAO IF WE WANT MPI VERSION
+# ADD MOUDLE PATH:   -DCMAKE_MODULE_PATH='~/Modules'           //DEFAULT IS ~/lib/Modules
+# USE MAGMA OR NOT:  -DUSE_MAGMA=on
+# EXAMPLE:
+#  os.environ['MAGMA'] ="/ccs/home/hshi/magma/magma-1.6.1"
+#  com="cmake -DCMAKE_CXX_COMPILER=CC \
+#             -DCMAKE_CXX_FLAGS='-Wall -O3 -fopenmp' \
+#             -DCOMPILER_EXTRA_DEF:STRING='-DMPI_HAO' \
+#             -DUSE_MAGMA=on \
+#             -DCMAKE_MODULE_PATH='~/cmake/Modules' \
+#             -DCMAKE_INSTALL_PREFIX:PATH="+install_dirc_name+" "+src_path
+#===============================================================================================================
+
+#Read from command line
 typ  = sys.argv[1]
 src_path = sys.argv[2]
 if( len(sys.argv)<=3 ):
@@ -10,43 +29,40 @@ if( len(sys.argv)<=3 ):
 else:
   install_dirc_name=sys.argv[3]
 
+#Different types
 if(typ=="mpi"):
   os.environ['LIBHAO']           = "~/lib/lib_hao/mpi"
   os.environ['GHF_HUBBARD_FFTW'] = "~/lib/ghf_hubbard_fftw/mpi"
   com="cmake -DCMAKE_CXX_COMPILER=mpic++ \
+             -DCMAKE_CXX_FLAGS='-Wall -O3'\
              -DCOMPILER_EXTRA_DEF:STRING='-DMPI_HAO' \
-             -DMODULE_EXTRA_PATH:STRING='~/lib/Modules' \
              -DCMAKE_INSTALL_PREFIX:PATH="+install_dirc_name+" "+src_path
 elif(typ=="serial"):
   os.environ['LIBHAO'] = "~/lib/lib_hao/serial"
   os.environ['GHF_HUBBARD_FFTW'] = "~/lib/ghf_hubbard_fftw/serial"
-  com="cmake -DCOMPILER_EXTRA_DEF:STRING=' '\
-             -DMODULE_EXTRA_PATH:STRING='~/lib/Modules' \
+  com="cmake -DCMAKE_CXX_FLAGS='-Wall -O3' \
              -DCMAKE_INSTALL_PREFIX:PATH="+install_dirc_name+" "+src_path
 elif(typ=="storm"):
   os.environ['LIBHAO'] = "~/lib/lib_hao/storm"
   os.environ['SPRNG'] = "~/sprng2.0"
   com="cmake -DCMAKE_CXX_COMPILER=mpicxx \
-             -DCMAKE_CXX_FLAGS='-Wall -O3 -std=c++11' \
+             -DCMAKE_CXX_FLAGS='-Wall -O3' \
              -DCOMPILER_EXTRA_DEF:STRING='-DMPI_HAO' \
-             -DMODULE_EXTRA_PATH:STRING='~/lib/Modules' \
              -DCMAKE_INSTALL_PREFIX:PATH="+install_dirc_name+" "+src_path
 elif(typ=="hu"):
   os.environ['LIBHAO'] = "~/lib/lib_hao/hu"
   os.environ['SPRNG'] = "~/sprng2.0"
   com="cmake -DCMAKE_CXX_COMPILER=mpicxx \
-             -DCMAKE_CXX_FLAGS='-Wall -O3 -march=corei7 -m64 -std=c++11' \
+             -DCMAKE_CXX_FLAGS='-Wall -O3 -march=corei7 -m64' \
              -DCOMPILER_EXTRA_DEF:STRING='-DMPI_HAO' \
-             -DMODULE_EXTRA_PATH:STRING='~/lib/Modules' \
              -DCMAKE_INSTALL_PREFIX:PATH="+install_dirc_name+" "+src_path
 elif(typ=="humagma"):
   os.environ['LIBHAO'] = "~/lib/lib_hao/humagma"
   os.environ['SPRNG'] = "~/sprng2.0"
   com="cmake -DCMAKE_CXX_COMPILER=mpicxx \
-             -DCMAKE_CXX_FLAGS='-Wall -O3 -march=corei7 -m64 -std=c++11' \
+             -DCMAKE_CXX_FLAGS='-Wall -O3 -march=corei7 -m64' \
              -DCOMPILER_EXTRA_DEF:STRING='-DMPI_HAO' \
              -DUSE_MAGMA=on \
-             -DMODULE_EXTRA_PATH:STRING='~/lib/Modules' \
              -DCMAKE_INSTALL_PREFIX:PATH="+install_dirc_name+" "+src_path
 elif(typ=="titan"):
   os.environ['LIBHAO'] = "~/lib_hao/mpi"
@@ -54,9 +70,9 @@ elif(typ=="titan"):
   os.environ['FFTW']  = "/opt/fftw/3.3.4.0/interlagos"
   os.environ['ACML']  = "/opt/acml/5.3.1/gfortran64"
   com="cmake -DCMAKE_CXX_COMPILER=CC \
-             -DCMAKE_CXX_FLAGS='-Wall -O3 -fopenmp -std=c++11' \
+             -DCMAKE_CXX_FLAGS='-Wall -O3 -fopenmp' \
              -DCOMPILER_EXTRA_DEF:STRING='-DMPI_HAO' \
-             -DMODULE_EXTRA_PATH:STRING='~/cmake/Modules' \
+             -DCMAKE_MODULE_PATH='~/cmake/Modules' \
              -DCMAKE_INSTALL_PREFIX:PATH=~/lib_hao/mpi "+src_path
 elif(typ=="titanmagma"):
   os.environ['LIBHAO'] = "~/lib_hao/mpimagma"
@@ -65,10 +81,10 @@ elif(typ=="titanmagma"):
   os.environ['ACML']  = "/opt/acml/5.3.1/gfortran64"
   os.environ['MAGMA'] ="/ccs/home/hshi/magma/magma-1.6.1"
   com="cmake -DCMAKE_CXX_COMPILER=CC \
-             -DCMAKE_CXX_FLAGS='-Wall -O3 -fopenmp -std=c++11' \
+             -DCMAKE_CXX_FLAGS='-Wall -O3 -fopenmp' \
              -DCOMPILER_EXTRA_DEF:STRING='-DMPI_HAO' \
              -DUSE_MAGMA=on \
-             -DMODULE_EXTRA_PATH:STRING='~/cmake/Modules' \
+             -DCMAKE_MODULE_PATH='~/cmake/Modules' \
              -DCMAKE_INSTALL_PREFIX:PATH=~/lib_hao/mpimagma "+src_path
 
 subprocess.call(com, shell=True )
